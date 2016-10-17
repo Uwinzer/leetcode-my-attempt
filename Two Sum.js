4a49274b5d9f3f79ee9a991afbdebb7049b31ca3
Uwinzer <lize19900315@gmail.com>
2016-10-17 00:16:12 -0400

    Add files via upload

--- Two Sum.js
+++ Two Sum.js
@@ -1,25 +1,20 @@
-var twoSum = function(nums, target) 
-{
+var twoSum = function(nums, target){
 var temp
-
-for (var i =0; i<nums.length;++i)
-{
-
+for (var i =0; i<nums.length;++i){
 		temp=target-nums[i]
-	
-	for (var j =0; j<nums.length;++j)
-		{
+	for (var j =0; j<nums.length;++j){
 
-			if (temp==nums[j])
-				{
-					
-					if(i!==j)
-					{
+			if (temp==nums[j]){
+					if(i!==j){
 						return [i,j]
-					}
-					
-				}
-			
+					}	
+			}	
 		}
 	}
-};
+}
+
+arr=[2,5,6,9]
+tar=14
+console.log(twoSum(arr,tar))
+
+
