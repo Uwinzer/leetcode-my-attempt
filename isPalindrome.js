4a74ae704b844f9f53f3719b0931d4f46a1025d1
zed <lize19900315@gmail.com>
2016-10-21 14:06:22 -0400

    Add files via upload

---
+++ isPalindrome.js
@@ -0,0 +1,29 @@
+/**
+ * Created by ZL on 2016-10-21.
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    var temp;
+    var flag=1;
+
+    if(~isNaN(x)&&x>=0){
+       
+        if(x<=Math.pow(2,31)){
+            temp=x.toString();
+            for(var i=0;i<=temp.length/2;i++){
+
+                if(temp[i]!=temp[temp.length-1-i]){
+
+                    flag=0;
+                }
+            }
+        }
+
+    }else{
+            flag=0;
+    }
+
+  return Boolean(flag)
+ 
+};
