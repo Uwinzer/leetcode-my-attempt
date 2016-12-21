e27f2e91b0dd9135dc9213de9257cf98c3a62fec
zed <lize19900315@gmail.com>
2016-12-21 14:44:41 -0500

    Add files via upload

--- Merge Tow Sorted List.js
+++ Merge Tow Sorted List.js
@@ -42,7 +42,52 @@ function LinkedList() {
     }
 
 }
+
+function showLinkedList(node) {
+    var arr=[];
+    while(node.next){
+        arr.push(node.value);
+        arr.val=arr.next;
+    }
+    console.log(arr)
+}
+var firstLinkedList=new LinkedList();
+firstLinkedList.append(1);
+firstLinkedList.append(2);
+firstLinkedList.append(3);
+
+var sectLinkedList=new LinkedList();
+sectLinkedList.append(2);
+sectLinkedList.append(3);
+sectLinkedList.append(4);
+
+
 var mergeTwoLists = function(l1, l2) {
+
+    var result=new ListNode(0);
+    var current =result;
+
+    while(l1!=null&&l2!=null){
+        if(l1.val<l2.val){
+            current.next=l1;
+            l1.val=l1.next
+        }else{
+            current.next=l2;
+            l2=l2.next;
+        }
+        current=current.next;
+
+
+    }
+    if(l1 !== null){
+        current.next = l1;
+    }
+
+    if(l2 !== null){
+        current.next = l2;
+    }
+    return result.next;
 
 
 };
+mergeTwoLists(firstLinkedList.head,sectLinkedList.head);
